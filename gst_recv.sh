de054968e3e6bec3c995ba454bc0ac4608cf6470
Charles Huber <genpfault@gmail.com>
2012-05-18 00:19:28 -0500

    Add timestamp overlay
    No millisecond timestamps :(

--- gst_recv.sh
+++ gst_recv.sh
@@ -4,5 +4,6 @@ udpsrc port=10000 \
 ! mpegtsdemux \
 ! ffdec_h264 \
 ! ffmpegcolorspace \
+! clockoverlay auto-resize=false shaded-background=true xpad=0 ypad=0 deltay=31  \
 ! videoscale method=0 add-borders=true \
 ! ximagesink sync=false
--- gst_xmit.sh
+++ gst_xmit.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
+#v4l2src device=/dev/video0 always-copy=false
 gst-launch -vvv \
-v4l2src device=/dev/video0 always-copy=false \
+videotestsrc is-live=true \
 ! video/x-raw-yuv,width=320,height=240,framerate=30/1 \
 ! videoscale method=0 \
 ! video/x-raw-yuv,width=160,height=120 \
+! clockoverlay auto-resize=false shaded-background=true xpad=0 ypad=0 deltay=0 \
 ! x264enc \
 tune=zerolatency \
 intra-refresh=true \
 profile=high \
 byte-stream=true \
 bitrate=220 \
+sliced-threads=true \
+speed-preset=2 \
 ! mpegtsmux \
 ! udpsink port=10000 host=127.0.0.1 ts-offset=0 sync=false
